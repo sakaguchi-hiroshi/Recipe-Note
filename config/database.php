28a61676c5a7c25be6071601df6176b454f50923
sakaguchi-hiroshi <nohoho0320@icloud.com>
2023-03-19 08:44:06 +0000

    model_controller-commit

--- config/database.php
+++ config/database.php
@@ -124,7 +124,20 @@ return [
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
-        ],
+	],
+
+	'clusters' => [
+            'default' => [
+                [
+                    'host' => env('REDIS_HOST', '127.0.0.1'),
+                    'password' => env('REDIS_PASSWORD', null),
+                    'port' => env('REDIS_PORT', 6379),
+                    'database' => 0,
+                    'read_write_timeout' => env('REDIS_READ_WRITE_TIMEOUT', 60),
+                ]
+            ],
+	],
+
         'default' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', 'recipe-note-cluster.gfg0pn.clustercfg.apne1.cache.amazonaws.com:6379'),
