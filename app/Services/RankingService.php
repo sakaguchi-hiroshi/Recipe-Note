6e910a46460d258e5138e21e9570288d627b0ac6
sakaguchi-hiroshi <nohoho0320@icloud.com>
2023-03-19 17:39:38 +0000

    RankingService-commit

--- app/Services/RankingService.php
+++ app/Services/RankingService.php
@@ -2,37 +2,45 @@
 namespace app\Services;
 
 use Illuminate\Http\Request;
-use Redis;
+use Illuminate\Support\Facades\Redis;
 
 class RankingService
 {
+    protected $redis;
 
-  public function incrementViewRanking($id)
-  {
-    $key = "ranking-"."id:".$id;
-    $value = Redis::get($key);
-
-    if(empty($value)) {
-      Redis::set($key, "1");
-      Redis::expire($key, 3600 * 24);
-    } else {
-      Redis::set($key, $value + 1);
+    public function __construct()
+    {
+        $this->redis = Redis::connection();
+        $this->redis->connect('recipe-note-cluster.gfg0pn.clustercfg.apne1.cache.amazonaws.com:6379', 6379);
     }
-  }
 
-  public function getRankingAll()
-  {
-    $keys = Redis::keys('ranking-*');
-    $results = Array();
+    public function incrementViewRanking($id)
+    {
+        $key = "ranking-"."id:".$id;
+        $value = $this->redis->get($key);
 
-    if(!empty($keys)) {
-      for($i = 0; $i < sizeof($keys); $i++) {
-        $id = explode(':', $keys[$i])[1];
-        $point = Redis::get('ranking-id:'.$id);
-        $results[$id] = $point;
-      }
-      arsort($results, SORT_NUMERIC);
+        if(empty($value)) {
+            $this->redis->set($key, "1");
+            $this->redis->expire($key, 3600 * 24);
+        } else {
+            $this->redis->set($key, $value + 1);
+        }
     }
-    return $results;
-  }
+
+    public function getRankingAll()
+    {
+        $keys = $this->redis->keys('ranking-*');
+        $results = Array();
+
+        if(!empty($keys)) {
+            for($i = 0; $i < sizeof($keys); $i++) {
+                $id = explode(':', $keys[$i])[1];
+                $point = $this->redis->get('ranking-id:'.$id);
+                $results[$id] = $point;
+            }
+            arsort($results, SORT_NUMERIC);
+        }
+        return $results;
+    }
 }
+
--- config/database.php
+++ config/database.php
@@ -123,7 +123,9 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+	    'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+	    'retry_limit' => 10,
+	    'retry_interval' => 100,
 	],
 
 	'clusters' => [
@@ -140,7 +142,7 @@ return [
 
         'default' => [
             'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', 'recipe-note-cluster.gfg0pn.clustercfg.apne1.cache.amazonaws.com:6379'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
